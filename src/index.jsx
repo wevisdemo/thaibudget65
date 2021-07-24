9c762103b65d99f4bd4f2cbf4c2b752e5e1a1468
Tae Prasongpongchai <taepras@gmail.com>
2021-07-24 16:16:09 +0700

    fix github pages routing issue

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   useLocation,
 } from 'react-router-dom';
