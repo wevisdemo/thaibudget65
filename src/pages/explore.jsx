a665ed0cd06bf8cf2e673a5cc32a64d3b6066c21
palminister <thanapatj.th@gmail.com>
2022-05-25 04:35:08 +0700

    Add explore page

---
+++ src/pages/explore.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import WvFooter from '@wevisdemo/ui/components/footer';
+
+const Explore = () => (
+  <div className="bg-[hsl(0,0%,98%)] text-black p-2">
+    <h1 className="wv-font-anuphan text-4xl font-bold">สำรวจงบประมาณปี 2566</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fames
+      velit eget bibendum augue et, sit nisl.
+    </p>
+    <WvFooter />
+  </div>
+);
+
+export default Explore;
