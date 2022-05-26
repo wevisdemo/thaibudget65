a5b77435d111fb9d9ea6c45f5032f69bedf46211
palminister <thanapatj.th@gmail.com>
2022-05-26 14:31:50 +0700

    Update route paths

--- src/index.jsx
+++ src/index.jsx
@@ -23,18 +23,18 @@ ReactDOM.render(
         href="https://design-systems.wevis.info/typography.css"
       />
       <div className="z-40 relative">
-        <WvNavbar title="THAILAND BUDGET 2566">
-          <Link to="/">
+        <WvNavbar title="THAILAND BUDGET 2566" homeHref={`${process.env.PUBLIC_URL}/`}>
+          <Link to={`${process.env.PUBLIC_URL}/`}>
             <WvNavButton>Treemap</WvNavButton>
           </Link>
-          <Link to="/explore">
+          <Link to={`${process.env.PUBLIC_URL}/explore`}>
             <WvNavButton>Explore</WvNavButton>
           </Link>
         </WvNavbar>
       </div>
       <Switch>
-        <Route exact path="/"><App /></Route>
-        <Route path="/explore"><Explore /></Route>
+        <Route exact path={`${process.env.PUBLIC_URL}/`}><App /></Route>
+        <Route path={`${process.env.PUBLIC_URL}/explore`}><Explore /></Route>
       </Switch>
     </Router>
   </React.StrictMode>,
