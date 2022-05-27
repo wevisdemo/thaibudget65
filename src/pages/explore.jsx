6429ef810c1c83a1726e394fd0a980c0ae6f8e51
napatswift <napat.do@ku.th>
2022-05-27 12:02:24 +0700

    fixed pos button

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -27,6 +27,13 @@ const DOWNLOAD_ICON = (
     </defs>
   </svg>
 );
+const FEEDBACK_BUTTON_STRING = 'เสนอแนะติชม';
+const FEEDBACK_URL_STRING = 'https://airtable.com/shryu4errnlj1LWsM';
+const FEEDBACK_ICON = (
+  <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0.498749 14.7513V18.5013H4.24875L15.3087 7.44128L11.5587 3.69128L0.498749 14.7513ZM18.2087 4.54128C18.5987 4.15128 18.5987 3.52128 18.2087 3.13128L15.8687 0.791279C15.4787 0.401279 14.8487 0.401279 14.4587 0.791279L12.6287 2.62128L16.3787 6.37128L18.2087 4.54128Z" fill="#3904E9" />
+  </svg>
+);
 
 const Explore = () => {
   const [allItems, setAllItems] = useState([]);
@@ -53,25 +60,41 @@ const Explore = () => {
   const result = filter(selectedKeywords[activeKeywordIndex].word, allItems);
 
   return (
-    <div className="bg-[hsl(0,0%,98%)] text-black px-4 xl:px-36 pt-6 md:pt-[73px] gap-y-8">
-      <h1 className="wv-font-anuphan text-4xl font-bold">{PAGE_TITLE_STRING}</h1>
-      <p className="text-[#828282] wv-font-anuphan mt-3">{PAGE_SUB_TITLE_STRING}</p>
-      <div className="wv-font-anuphan flex p-6 justify-between items-center bg-[#3904E90A] rounded-xl mt-8">
-        <p className="text-xl">{`จากงบประมาณทั้งหมด ${result && (result.totalYearBudget / 1_000_000).toLocaleString()} ล้านบาท`}</p>
-        <a
-          type="button"
-          className="inline-flex space-x-3 items-center rounded-md bg-[#3904E9] text-white p-4"
-          href={DOWNLOAD_DATA_URL_STRING}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {DOWNLOAD_ICON}
-          <span>{DOWNLOAD_DATA_BUTTON_STRING}</span>
-        </a>
-      </div>
-      <div className="wv-font-anuphan grid grid-cols-1 md:grid-cols-3 gap-6 my-7">
-        <KeywordList keywords={selectedKeywords} activeKeyword={selectedKeywords[activeKeywordIndex].word} onActiveKeywordIndex={setActiveKeywordIndex} />
-        <Result result={result} keyword={selectedKeywords[activeKeywordIndex].word} />
+    <div>
+      <div className="bg-[hsl(0,0%,98%)] text-black px-4 xl:px-36 py-6 md:pt-[73px] gap-y-8 relative">
+        <div>
+          <a
+            type="button"
+            className="wv-font-anuphan flex items-center space-x-3 rounded-md text-[#3904E9] bg-white shadow-xl p-4 fixed z-50 bottom-6 right-10"
+            href={FEEDBACK_URL_STRING}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {FEEDBACK_ICON}
+            <span>
+              {FEEDBACK_BUTTON_STRING}
+            </span>
+          </a>
+        </div>
+        <h1 className="wv-font-anuphan text-4xl font-bold">{PAGE_TITLE_STRING}</h1>
+        <p className="text-[#828282] wv-font-anuphan mt-3">{PAGE_SUB_TITLE_STRING}</p>
+        <div className="wv-font-anuphan flex p-6 justify-between items-center bg-[#3904E90A] rounded-xl mt-8">
+          <p className="text-xl">{`จากงบประมาณทั้งหมด ${result && (result.totalYearBudget / 1_000_000).toLocaleString()} ล้านบาท`}</p>
+          <a
+            type="button"
+            className="inline-flex space-x-3 items-center rounded-md bg-[#3904E9] text-white p-4"
+            href={DOWNLOAD_DATA_URL_STRING}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {DOWNLOAD_ICON}
+            <span>{DOWNLOAD_DATA_BUTTON_STRING}</span>
+          </a>
+        </div>
+        <div className="wv-font-anuphan grid grid-cols-1 md:grid-cols-3 gap-6 my-7">
+          <KeywordList keywords={selectedKeywords} activeKeyword={selectedKeywords[activeKeywordIndex].word} onActiveKeywordIndex={setActiveKeywordIndex} />
+          <Result result={result} keyword={selectedKeywords[activeKeywordIndex].word} />
+        </div>
       </div>
       <WvFooter />
     </div>
