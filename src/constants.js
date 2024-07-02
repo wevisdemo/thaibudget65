9940965559912bf4689f4ee3015e11c4d4a1a679
Th1nkK1D <witheep@gmail.com>
2024-07-02 15:30:52 +0700

    fix: update year constants and convert amount in 68 csv to number

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
-export const CURRENT_FISCAL_YEAR = 2567;
-export const CURRENT_FISCAL_YEAR_SHORT = 67;
-export const CURRENT_FISCAL_YEAR_TOTAL_BUDGET = 3_479_999_969_500.0;
+export const CURRENT_FISCAL_YEAR = 2568;
+export const CURRENT_FISCAL_YEAR_SHORT = 68;
 export const CURRENT_DATA_URL =
   'https://docs.google.com/spreadsheets/d/1_gqyl1IDwNXvTKkqQU6hPW7wLvRoljzLjpPsN2llZek/edit#gid=1392569138';
 export const PREV_DATA_URL =
