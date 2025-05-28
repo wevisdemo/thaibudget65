d9d26121b1e748158a76daa9460e77669688dde8
Th1nkK1D <witheep@gmail.com>
2025-05-28 11:15:26 +0300

    feat: update 69 data and remove unused 67

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const CURRENT_FISCAL_YEAR = 2569;
 export const CURRENT_FISCAL_YEAR_SHORT = 69;
 export const CURRENT_DATA_URL =
-  'https://docs.google.com/spreadsheets/d/1oRnzLwK2VYS-BjbZjs5Csl9_PtT0YeUWiYCjxhFVVl0/edit#gid=1392569138';
-export const CURRENT_DATA_SHEET_NAME = '2025-05-22';
+  'https://docs.google.com/spreadsheets/d/1oRnzLwK2VYS-BjbZjs5Csl9_PtT0YeUWiYCjxhFVVl0/edit';
+export const CURRENT_DATA_SHEET_NAME = '2025-05-25#';
 export const PREV_DATA_URL =
-  'https://docs.google.com/spreadsheets/d/1_gqyl1IDwNXvTKkqQU6hPW7wLvRoljzLjpPsN2llZek/edit#gid=1392569138';
+  'https://docs.google.com/spreadsheets/d/1_gqyl1IDwNXvTKkqQU6hPW7wLvRoljzLjpPsN2llZek/edit';
